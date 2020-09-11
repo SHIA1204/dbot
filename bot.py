4e14f56770fbaf4de0ba88406697e4125e537d9e
Nyaru-chan <59565202+SHIA1204@users.noreply.github.com>
2020-09-11 18:13:16 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -793,8 +793,14 @@ async def search_youtube(ctx, *, arg):
     arg_title = str(arg)
     arg = str(arg).replace(" ", "%20")
     
-    browser = webdriver.Chrome('chromedriver.exe')
-    browser.maximize_window()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+#    browser.maximize_window()
 
     url = f"https://www.youtube.com/results?search_query={arg}"
 #    headers = {
