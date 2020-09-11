55927c0b48d54503878a8de093f33f3cb66c34b6
Nyaru-chan <59565202+SHIA1204@users.noreply.github.com>
2020-09-11 17:52:10 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -793,7 +793,7 @@ async def search_youtube(ctx, *, arg):
     arg_title = str(arg)
     arg = str(arg).replace(" ", "%20")
     
-    browser = webdriver.Chrome('SHIA1204/dbot/chromedriver.exe')
+    browser = webdriver.Chrome('chromedriver.exe')
     browser.maximize_window()
 
     url = f"https://www.youtube.com/results?search_query={arg}"
